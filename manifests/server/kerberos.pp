43b8d580224ab3600b03e2a4680c1983a86d61aa
Michael Johnson - MJ <mj@revmj.com>
2011-08-16 11:29:38 -0700

    Having to break things apart a bit.  Puppet really should allow you to stop, start, and restart a service multiple times.  Have to forcibly kill slapd after configuring it because once I change the pid file, the stop script not longer works.  Sigh.

--- manifests/server/kerberos.pp
+++ manifests/server/kerberos.pp
@@ -1,38 +1,82 @@
 define suiteds::server::kerberos (
-  $ensure         = $suiteds::config::ensure,
-  $user           = $suiteds::config::root_user,
-  $group          = $suiteds::config::root_group,
-  $krb_ldap_write = $suiteds::config::krb_ldap_write,
-  $krb_ldap_read  = $suiteds::config::krb_ldap_read,
-  $krb_path       = $suiteds::config::krb_path,
-  $admin_password = $suiteds::config::admin_password,
-  $exec_path      = $suiteds::config::exec_path
-) {
-  $db_mapping   = $suiteds::config::db_mapping
-  $domain       = inline_template( '<%= name.to_s.downcase.strip %>' )
-  $base_dn      = inline_template( '<%= domain.split( "." ).map{ |part| part = "dc=" + part }.join( "," ) %>' )
-  $admin_ou     = inline_template( '<%= "ou=" + db_mapping.select{ |x| x.split( ":" ).at( 0 ).to_s.strip == "admin" }.at( 0 ).split( ":" ).at( 4 ).to_s.strip %>' )
-  $krb_ou       = inline_template( '<%= "ou=" + db_mapping.select{ |x| x.split( ":" ).at( 0 ).to_s.strip == "kerberos" }.at( 0 ).split( ":" ).at( 4 ).to_s.strip %>' )
-  $realm        = inline_template( '<%= name.to_s.upcase %>' )
-  $krb_write_dn = "cn=${krb_ldap_write},${admin_ou},${base_dn}"
-  $secret_file  = "${krb_path}/${domain}.secret"
-  $secret       = inline_template( '<%= Array.new( 64 ){ rand( 256 ).chr }.join.unpack( "H*" ).join%>' )
+  $ensure                = $suiteds::config::ensure,
+  $servers               = $suiteds::config::servers,
+  $domains               = $suiteds::config::domains,
+  $default_domain        = $suiteds::config::default_domain,
+  $admin_user            = $suiteds::config::admin_user,
+  $admin_password        = $suiteds::config::admin_password,
+  $base_path             = $suiteds::config::base_path,
+  $config_path           = $suiteds::config::config_path,
+  $misc_path             = $suiteds::config::misc_path,
+  $exec_path             = $suiteds::config::exec_path,
+  $pam_min_uid           = $suiteds::config::pam_min_uid,
+  $pam_max_uid           = $suiteds::config::pam_max_uid,
+  $log_level             = $suiteds::config::log_level,
+  
+  $ldap_protocols        = $suiteds::config::ldap_protocols,
+  $ldap_default_protocol = $suiteds::config::ldap_default_protocol,
+  $ldap_version          = $suiteds::config::ldap_version,
+  $ldap_port             = $suiteds::config::ldap_port,
+  $ldaps_port            = $suiteds::config::ldaps_port,
+  $search_timelimit      = $suiteds::config::search_timelimit,
+  $bind_timelimit        = $suiteds::config::bind_timelimit,
+  $idle_timelimit        = $suiteds::config::idle_timelimit,
 
-  $exec_krb_init    = "cat '${secret_file}' | kdb5_ldap_util -D '${krb_write_dn}' create -subtrees '${base_dn}' -r '${realm}' -s"
-  $exec_krb_is_init = "test -n \"`ldapsearch -Y EXTERNAL -H ldapi:/// -LLL -Q -b '${krb_ou},${base_dn}' '(krbPrincipalName=K/M@${realm})' dn`\""
+  $ssl_mode              = $suiteds::config::ssl_mode,
+  $ssl_minimum           = $suiteds::config::ssl_minimum,
+  $ssl_verify_certs      = $suiteds::config::ssl_verify_certs,
+
+  $krb5_port             = $suiteds::config::krb5_port,
+  $krb5adm_port          = $suiteds::config::krb5adm_port,
+  $krb4_port             = $suiteds::config::krb4_port
+) {
+  # Check to see if we have been called previously by utilizing as dummy
+  # resource.
+  if( defined( Suiteds::Dummy[ 'suiteds::server::kerberos' ] ) ) {
+    fail( 'The "suiteds::server::kerberos" define has already been called previously in your manifest!' )
+  }
+  suiteds::dummy{ 'suiteds::server::kerberos': }
+
+  # Include our config.
+  include suiteds::config
+
+  $temp_krb_path = $suiteds::config::krb_path
+  case inline_template( '<%= temp_krb_path.to_s.start_with?( "/" ) %>' ) {
+    'true':  { $krb_path = $temp_krb_path                }
+    default: { $krb_path = "${base_path}/$temp_krb_path" }
+  }
+
+  $packages       = $suiteds::config::krb_server_packages
+  $services       = $suiteds::config::krb_server_services
+  $configs        = $suiteds::config::krb_server_configs
+  $root_user      = $suiteds::config::root_user
+  $root_group     = $suiteds::config::root_group
+  $db_mapping     = $suiteds::config::db_mapping
+  $krb_ldap_read  = $suiteds::config::krb_ldap_read
+  $krb_ldap_write = $suiteds::config::krb_ldap_write
 
   case $ensure {
     'present': {
       $file_ensure = $ensure
 
-      exec{ "krb_init-${realm}":
-        command   => $exec_krb_init,
-        path      => $exec_path,
-        user      => $user,
-        group     => $group,
-        logoutput => 'on_failure',
-        unless    => $exec_krb_is_init,
-        require   => File[ $secret_file ],
+      package{ $packages:
+        ensure  => $ensure,
+        require => Directory[ $krb_path ],
+      }
+
+      suiteds::toggle{ $configs:
+        ensure  => $ensure,
+        require => Package[ $packages ],
+        #before  => Suiteds::Server::Kerberos::Realm[ $domains ],
+      }
+
+      #suiteds::server::kerberos::realm{ $domains:
+      #}
+
+      service{ $services:
+        ensure  => 'running',
+        enable  => 'true',
+        #require => Suiteds::Server::Kerberos::Realm[ $domains ],
       }
     }
     'absent','purged': {
@@ -43,11 +87,11 @@ define suiteds::server::kerberos (
     }
   }
 
-  file{ $secret_file:
-    ensure  => $file_ensure,
-    owner   => $user,
-    group   => $group,
-    mode    => '0600',
-    content => inline_template( '<%= admin_password + "\n" + secret + "\n" + secret %>' )
+  directory{ $krb_path:
+    ensure  => $ensure,
+    recurse => 'true',
+    owner   => $root_user,
+    group   => $root_group,
+    mode    => 0700,
   }
 }
---
+++ manifests/server/openldap.pp
@@ -0,0 +1,187 @@
+define suiteds::server::openldap (
+  $ensure                = $suiteds::config::ensure,
+  $servers               = $suiteds::config::servers,
+  $domains               = $suiteds::config::domains,
+  $default_domain        = $suiteds::config::default_domain,
+  $admin_user            = $suiteds::config::admin_user,
+  $admin_password        = $suiteds::config::admin_password,
+  $base_path             = $suiteds::config::base_path,
+  $config_path           = $suiteds::config::config_path,
+  $misc_path             = $suiteds::config::misc_path,
+  $exec_path             = $suiteds::config::exec_path,
+  $pam_min_uid           = $suiteds::config::pam_min_uid,
+  $pam_max_uid           = $suiteds::config::pam_max_uid,
+  $log_level             = $suiteds::config::log_level,
+
+  $ldap_protocols        = $suiteds::config::ldap_protocols,
+  $ldap_default_protocol = $suiteds::config::ldap_default_protocol,
+  $ldap_version          = $suiteds::config::ldap_version,
+  $ldap_port             = $suiteds::config::ldap_port,
+  $ldaps_port            = $suiteds::config::ldaps_port,
+  $bind_policy           = $suiteds::config::bind_policy,
+  $search_timelimit      = $suiteds::config::search_timelimit,
+  $bind_timelimit        = $suiteds::config::bind_timelimit,
+  $idle_timelimit        = $suiteds::config::idle_timelimit,
+  $sasl_minssf           = $suiteds::config::sasl_minssf,
+  $sasl_maxssf           = $suiteds::config::sasl_maxssf,
+
+  $ssl_mode              = $suiteds::config::ssl_mode,
+  $ssl_minimum           = $suiteds::config::ssl_minimum,
+  $ssl_verify_certs      = $suiteds::config::ssl_verify_certs,
+  $ssl_cacert_file       = $suiteds::config::ssl_cacert_file,
+  $ssl_cacert_path       = $suiteds::config::ssl_cacert_path,
+  $ssl_cert_file         = $suiteds::config::ssl_cert_file,
+  $ssl_key_file          = $suiteds::config::ssl_key_file,
+  $ssl_cipher_suite      = $suiteds::config::ssl_cipher_suite
+) {
+  # Check to see if we have been called previously by utilizing as dummy
+  # resource.
+  if( defined( Suiteds::Dummy[ 'suiteds::server::openldap' ] ) ) {
+    fail( 'The "suiteds::server::openldap" define has already been called previously in your manifest!' )
+  }
+  suiteds::dummy{ 'suiteds::server::openldap': }
+
+  # Include our config.
+  include suiteds::config
+
+  # Make sure our paths are fully qualified.
+  $temp_ldap_path = $suiteds::config::ldap_path
+  case inline_template( '<%= temp_ldap_path.to_s.start_with?( "/" ) %>' ) {
+    'true':  { $ldap_path = $temp_ldap_path                       }
+    default: { $ldap_path = "${config_base_path}/$temp_ldap_path" }
+  }
+  $temp_ldap_pid_file = $suiteds::config::ldap_pid_file
+  case inline_template( '<%= temp_ldap_pid_file.to_s.start_with?( "/" ) %>' ) {
+    'true':  { $ldap_pid_file = $temp_ldap_pid_file               }
+    default: { $ldap_pid_file = "${ldap_path}/${temp_ldap_pid_file}" }
+  }
+
+  $packages       = $suiteds::config::ldap_server_packages
+  $services       = $suiteds::config::ldap_server_services
+  $ldap_user      = $suiteds::config::ldap_user
+  $ldap_group     = $suiteds::config::ldap_group
+  $root_user      = $suiteds::config::root_user
+  $root_group     = $suiteds::config::root_group
+  $ldap_schemas   = $suiteds::config::ldap_schemas
+  $db_mapping     = $suiteds::config::db_mapping
+  $krb_ldap_read  = $suiteds::config::krb_ldap_read
+  $krb_ldap_write = $suiteds::config::krb_ldap_write
+
+  $ldap_srv_init_file = "${misc_path}/server-init.ldif"
+  $ldap_srv_pop_file  = "${misc_path}/server-populate.ldif"
+  $ldap_dir_init_file = "${misc_path}/directory-init.ldif"
+  $ldap_dir_pop_file  = "${misc_path}/directory-populate.ldif"
+  $configs = [
+    $suiteds::config::ldap_server_configs,
+    "present:absent:${ldap_user}:${ldap_group}:0600:server/server-init.ldif       :${ldap_srv_init_file}",
+    "present:absent:${ldap_user}:${ldap_group}:0600:server/server-populate.ldif   :${ldap_srv_pop_file} ",
+    "present:absent:${ldap_user}:${ldap_group}:0600:server/directory-init.ldif    :${ldap_dir_init_file}",
+    "present:absent:${ldap_user}:${ldap_group}:0600:server/directory-populate.ldif:${ldap_dir_pop_file} ",
+  ]
+  $base_dn      = inline_template( '<%= domain.to_s.downcase.split( "." ).map{ |part| part = "dc=" + part }.join( "," ) %>' )
+
+  $exec_ldap_srv_init    = "slapadd -F '${ldap_path}/config' -d1 -n 0 -l '${ldap_srv_init_file}' 2>&1"
+  $exec_ldap_srv_is_init = "slapcat -F '${ldap_path}/config' -H ldap:///cn=schema,cn=config"
+
+  $exec_ldap_srv_pop     = "slapadd -F '${ldap_path}/config' -d1 -n 0 -l '${ldap_srv_pop_file}' 2>&1"
+  $exec_ldap_srv_is_pop  = "test -n \"`slapcat -F '${ldap_path}/config' -H ldap:///cn=schema,cn=config??one 2>/dev/null`\""
+
+  $exec_ldap_dir_init    = "ldapadd -Y EXTERNAL -H ldapi:/// -d1 -f '${ldap_dir_init_file}' 2>&1"
+  $exec_ldap_dir_is_init = "test -n \"`ldapsearch -Y EXTERNAL -H ldapi:/// -LLL -Q -b cn=config '(&(objectClass=olcDatabaseConfig)(olcSuffix=*))' dn`\""
+ 
+  $exec_ldap_dir_pop     = "ldapadd -Y EXTERNAL -H ldapi:/// -d1 -f '${ldap_dir_pop_file}' 2>&1"
+  $exec_ldap_dir_is_pop  = "test -n \"`ldapsearch -Y EXTERNAL -H ldapi:/// -LLL -Q -b '${base_dn}' '(ou=${base_dn})' dn`\""
+
+  # Set up some resource defaults.
+  Exec{
+    user      => $root_user,
+    group     => $root_group,
+    path      => $exec_path,
+    logoutput => 'on_failure',
+  }
+
+  directory{ $ldap_path:
+    ensure  => $ensure,
+    recurse => 'true',
+    owner   => $ldap_user,
+    group   => $ldap_group,
+    mode    => 0700,
+    require => Package[ $packages ],
+  }
+
+  case $ensure {
+    'present': {
+      package{ $packages:
+        ensure  => $ensure,
+        before  => Service[ $services ],
+      }
+      suiteds::server::mk_ldap_dir_paths{ [ 'config', $domains ]:
+        ensure    => $ensure,
+        user      => $ldap_user,
+        group     => $ldap_group,
+        mode      => 0700,
+        base_path => $ldap_path,
+        require   => Package[ $packages ],
+      }
+      suiteds::toggle{ $configs:
+        ensure  => $ensure,
+        require => Package[ $packages ],
+        before  => Exec[ 'ldap-srv-init' ],
+        notify  => Exec[ 'killall slapd || true' ],
+      }
+      exec{ 'killall slapd || true':
+        before      => Exec[ 'ldap-srv-init' ],
+        require     => Package[ $packages ],
+        refreshonly => 'true'
+      }
+      exec{ 'ldap-srv-init':
+        user      => $ldap_user,
+        group     => $ldap_group,
+        command   => $exec_ldap_srv_init,
+        require   => Package[ $packages ],
+        before    => Exec[ 'ldap-srv-pop' ],
+        unless    => $exec_ldap_srv_is_init,
+      }
+      exec{ 'ldap-srv-pop':
+        user      => $ldap_user,
+        group     => $ldap_group,
+        command   => $exec_ldap_srv_pop,
+        unless    => $exec_ldap_srv_is_pop,
+        notify    => Service[ $services ],
+      }
+
+      service{ $services:
+        ensure  => 'running',
+        enable  => 'true',
+      }
+
+      exec{ 'ldap-dir-init':
+        command     => $exec_ldap_dir_init,
+        require     => Service[ $services ],
+        unless      => $exec_ldap_dir_is_init,
+      }
+      exec{ 'ldap-dir-pop':
+        command     => $exec_ldap_dir_pop,
+        require     => Exec[ 'ldap-dir-init' ],
+        unless      => $exec_ldap_dir_is_pop,
+      }
+    }
+    'absent','purged': {
+      package{ $packages:
+        ensure  => $ensure,
+        before  => Directory[ $ldap_path ],
+      }
+      exec{ 'killall slapd || true':
+        before      => Service[ $services ],
+      }
+      service{ $services:
+        ensure  => 'stopped',
+        enable  => 'false',
+        before  => Package[ $packages ],
+      }
+    }
+    default: {
+      fail( "'$ensure' is not a valid value for 'ensure'" )
+    }
+  }
+}
